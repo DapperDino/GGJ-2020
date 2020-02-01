acf353ddbe38184d70a98c0b8a9f57276e51fb55
Sebastiaan Westheim <sebastiaan.westheim@hva.nl>
2020-02-01 04:01:01 +0100

    Created WorldGenateration.cs to create a random world around the starting zone.

---
+++ Assets/Scripts/WorldGeneration/Node.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Node
+{
+    public string Name { get; set; } = "";
+
+    // Null means it's the starting point.
+    public Node Parent { get; set; }
+    public RoomFlags RoomFlags { get; set; } = RoomFlags.None;
+    public GameObject GameObject { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+}
